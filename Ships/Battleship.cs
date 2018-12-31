b2b26576d4ece7977a0a9c50d2a45985f09d9a71
Daniil Grunin <grunindan@gmail.com>
2018-12-31 14:44:54 +0400

    Добавлена лабораторная работа №1

---
+++ Ships/Battleship.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+
+namespace Ships
+{
+    class Battleship
+    {
+        private const int _battleshipWidth = 230;
+        private const int _battleshipHeight = 60;
+
+        private float _startPosX;
+        private float _startPosY;
+
+        private int _pictureWidth;
+        private int _pictureHeight;
+
+        private Direction dirX = Direction.Right;
+
+        public int MaxSpeed { private set; get; }
+
+        public float Weight { private set; get; }
+
+        public Color Color { private set; get; }
+
+        public Battleship(int maxSpeed, float weight, Color color)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            Color = color;
+        }
+
+        public void SetPos(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void MoveBattleship(Direction dir)
+        {
+            if (dir == Direction.None) return;
+            float step = MaxSpeed * 100 / Weight;
+            switch (dir)
+            {
+                case Direction.Up:
+                    if (_startPosY - step > 0) _startPosY -= step;
+                    else _startPosY = 0;
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - _battleshipHeight) _startPosY += step;
+                    else _startPosY = _pictureHeight - _battleshipHeight;
+                    break;
+                case Direction.Left:
+                    dirX = Direction.Left;
+                    if (_startPosX - step > 0) _startPosX -= step;
+                    else _startPosX = 0;
+                    break;
+                case Direction.Right:
+                    dirX = Direction.Right;
+                    if (_startPosX + step < _pictureWidth - _battleshipWidth) _startPosX += step;
+                    else _startPosX = _pictureWidth - _battleshipWidth;
+                    break;
+            }
+        }
+
+        public void DrawBattleship(Graphics g)
+        {
+            int x = 0;
+            int sign = 0;
+            switch (dirX)
+            {
+                case (Direction.Left):
+                    x = _battleshipWidth;
+                    sign = -1;
+                    break;
+                case (Direction.Right):
+                    x = 0;
+                    sign = 1;
+                    break;
+            }
+            Brush brush = new SolidBrush(Color);
+            Point point1 = new Point((int)_startPosX + x, (int)_startPosY + 54);
+            Point point2 = new Point((int)_startPosX + x + sign * 10, (int)_startPosY + 60);
+            Point point3 = new Point((int)_startPosX + x + sign * 180, (int)_startPosY + 60);
+            Point point4 = new Point((int)_startPosX + x + sign * 190, (int)_startPosY + 54);
+            Point point5 = new Point((int)_startPosX + x + sign * 108, (int)_startPosY + 54);
+            Point point6 = new Point((int)_startPosX + x + sign * 100, (int)_startPosY + 50);
+            Point point7 = new Point((int)_startPosX + x + sign * 230, (int)_startPosY + 46);
+            Point point8 = new Point((int)_startPosX + x + sign * 230, (int)_startPosY + 44);
+            Point point9 = new Point((int)_startPosX + x + sign * 74, (int)_startPosY + 44);
+            Point point10 = new Point((int)_startPosX + x + sign * 66, (int)_startPosY + 30);
+            Point point11 = new Point((int)_startPosX + x + sign * 54, (int)_startPosY + 30);
+            Point point12 = new Point((int)_startPosX + x + sign * 48, (int)_startPosY + 42);
+            Point point13 = new Point((int)_startPosX + x + sign * 40, (int)_startPosY + 42);
+            Point point14 = new Point((int)_startPosX + x + sign * 34, (int)_startPosY + 54);
+            Point[] points = { point1, point2, point3, point4, point5, point6, point6, point7, point8, point9, point10, point11, point12, point13, point14 };
+            g.FillPolygon(brush, points);
+            Pen pen = new Pen(Color);
+            g.DrawLine(pen, (int)_startPosX + x + sign * 60, (int)_startPosY + 30,
+                (int)_startPosX + x + sign * 60, (int)_startPosY);
+        }
+    }
+}
---
+++ Ships/GameWindow.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Ships
+{
+    public partial class GameWindow : Form
+    {
+        private Battleship ship;
+
+        private System.Timers.Timer delayTimer;
+        private System.Timers.Timer moveTimer;
+
+        private Direction dirX = Direction.None;
+        private Direction dirY = Direction.None;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            CreateBattleship();
+            SetDelayTimer();
+            SetMoveTimer();
+        }
+
+        private void CreateBattleship()
+        {
+            Random rnd = new Random();
+            ship = new Battleship(rnd.Next(60, 100), rnd.Next(300, 500), Color.White);
+            ship.SetPos(rnd.Next(10, 100), rnd.Next(10, 100), drawingArea.Width, drawingArea.Height);
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(drawingArea.Width, drawingArea.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            ship.DrawBattleship(g);
+            drawingArea.Image = bmp;
+        }
+
+        private void SetDelayTimer()
+        {
+            delayTimer = new System.Timers.Timer(1000 / 60);
+            delayTimer.Elapsed += DelayTimerElapsed;
+            delayTimer.AutoReset = true;
+        }
+
+        private void SetMoveTimer()
+        {
+            moveTimer = new System.Timers.Timer(1000 / 3);
+            moveTimer.Elapsed += MoveTimerElapsed;
+            moveTimer.AutoReset = true;
+        }
+
+        private void DelayTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            moveTimer.Start();
+            MoveTimerElapsed(null, null);
+            delayTimer.Stop();
+        }
+
+        private void MoveTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            ship.MoveBattleship(dirX);
+            ship.MoveBattleship(dirY);
+            Draw();
+        }
+
+        private void GameWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case (Keys.Left): dirX = Direction.Left; break;
+                case (Keys.Right): dirX = Direction.Right; break;
+                case (Keys.Up): dirY = Direction.Up; break;
+                case (Keys.Down): dirY = Direction.Down; break;
+                default: return;
+            }
+            if (!delayTimer.Enabled && !moveTimer.Enabled)
+                delayTimer.Start();
+        }
+
+        private void GameWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && dirX == Direction.Left
+                || e.KeyCode == Keys.Right && dirX == Direction.Right)
+                dirX = Direction.None;
+            else if (e.KeyCode == Keys.Up && dirY == Direction.Up
+                || e.KeyCode == Keys.Down && dirY == Direction.Down)
+                dirY = Direction.None;
+            else return;
+            if (dirX == Direction.None && dirY == Direction.None) moveTimer.Stop();
+        }
+    }
+}
