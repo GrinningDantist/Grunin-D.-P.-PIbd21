9e3d28475034c02386a4eaa3d4c0756040a57700
Daniil Grunin <grunindan@gmail.com>
2019-01-09 21:37:44 +0400

    Добавлена лабораторная работа №2

--- Ships/GameWindow.Designer.cs
+++ Ships/GameWindow.Designer.cs
@@ -29,25 +29,59 @@
         private void InitializeComponent()
         {
             this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
             this.SuspendLayout();
             // 
             // drawingArea
             // 
-            this.drawingArea.BackColor = System.Drawing.Color.Black;
-            this.drawingArea.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.drawingArea.Location = new System.Drawing.Point(0, 0);
+            this.drawingArea.BackColor = System.Drawing.Color.Transparent;
+            this.drawingArea.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.drawingArea.Location = new System.Drawing.Point(0, 59);
             this.drawingArea.Name = "drawingArea";
-            this.drawingArea.Size = new System.Drawing.Size(800, 450);
+            this.drawingArea.Size = new System.Drawing.Size(800, 391);
             this.drawingArea.TabIndex = 0;
             this.drawingArea.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ВОЕННЫЙ КОРАБЛЬ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnWarship_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(125, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "КРЕЙСЕР";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnCruiser_Click);
             // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.drawingArea);
+            this.KeyPreview = true;
             this.Name = "GameWindow";
             this.Text = "Корабль";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameWindow_KeyDown);
@@ -60,6 +94,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ Ships/Warship.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    class Warship : Vehicle
+    {
+        protected const int _warshipWidth = 230;
+        protected const int _warshipHeight = 60;
+
+        public Warship(int maxSpeed, float weight, Color mainColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+        }
+
+        public override void MoveTransport(Direction dir)
+        {
+            if (dir == Direction.None) return;
+            float step = MaxSpeed * 100 / Weight;
+            switch (dir)
+            {
+                case Direction.Up:
+                    if (_startPosY - step > 0) _startPosY -= step;
+                    else _startPosY = 0;
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - _warshipHeight) _startPosY += step;
+                    else _startPosY = _pictureHeight - _warshipHeight;
+                    break;
+                case Direction.Left:
+                    dirX = Direction.Left;
+                    if (_startPosX - step > 0) _startPosX -= step;
+                    else _startPosX = 0;
+                    break;
+                case Direction.Right:
+                    dirX = Direction.Right;
+                    if (_startPosX + step < _pictureWidth - _warshipWidth) _startPosX += step;
+                    else _startPosX = _pictureWidth - _warshipWidth;
+                    break;
+            }
+        }
+
+        public override void DrawTransport(Graphics g)
+        {
+            int stemPosX = 0;
+            int sign = 0;
+            switch (dirX)
+            {
+                case (Direction.Left):
+                    stemPosX = _warshipWidth;
+                    sign = -1;
+                    break;
+                case (Direction.Right):
+                    stemPosX = 0;
+                    sign = 1;
+                    break;
+            }
+            Brush brush = new SolidBrush(MainColor);
+            Point point1 = new Point((int)_startPosX + stemPosX, (int)_startPosY + 54);
+            Point point2 = new Point((int)_startPosX + stemPosX + sign * 10, (int)_startPosY + 60);
+            Point point3 = new Point((int)_startPosX + stemPosX + sign * 180, (int)_startPosY + 60);
+            Point point4 = new Point((int)_startPosX + stemPosX + sign * 190, (int)_startPosY + 54);
+            Point point5 = new Point((int)_startPosX + stemPosX + sign * 108, (int)_startPosY + 54);
+            Point point6 = new Point((int)_startPosX + stemPosX + sign * 100, (int)_startPosY + 50);
+            Point point7 = new Point((int)_startPosX + stemPosX + sign * 230, (int)_startPosY + 46);
+            Point point8 = new Point((int)_startPosX + stemPosX + sign * 230, (int)_startPosY + 44);
+            Point point9 = new Point((int)_startPosX + stemPosX + sign * 74, (int)_startPosY + 44);
+            Point point10 = new Point((int)_startPosX + stemPosX + sign * 66, (int)_startPosY + 30);
+            Point point11 = new Point((int)_startPosX + stemPosX + sign * 54, (int)_startPosY + 30);
+            Point point12 = new Point((int)_startPosX + stemPosX + sign * 48, (int)_startPosY + 42);
+            Point point13 = new Point((int)_startPosX + stemPosX + sign * 40, (int)_startPosY + 42);
+            Point point14 = new Point((int)_startPosX + stemPosX + sign * 34, (int)_startPosY + 54);
+            Point[] points = { point1, point2, point3, point4, point5, point6, point6, point7, point8, point9, point10, point11, point12, point13, point14 };
+            g.FillPolygon(brush, points);
+            Pen pen = new Pen(MainColor);
+            g.DrawLine(pen, (int)_startPosX + stemPosX + sign * 60, (int)_startPosY + 30,
+                (int)_startPosX + stemPosX + sign * 60, (int)_startPosY);
+        }
+    }
+}
